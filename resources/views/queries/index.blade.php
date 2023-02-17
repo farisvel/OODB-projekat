97bc2b390d4dec64e58a8da6cececdedfa31479e
Faris Veladzic <faris.veladzic@gmail.com>
2023-02-17 23:23:14 +0100

    Sve ok

--- resources/views/queries/index.blade.php
+++ resources/views/queries/index.blade.php
@@ -9,35 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid grid-cols-4 gap-4 p-4 justify-items-center">
                 <div>
-                    <h1>Upit 1</h1>
+                    <h1>Upit 1: Pisci koji pisu zanr: Automobili</h1>
                     <hr/>
                     @foreach($pisci as $pisci)
                     <p>{{$loop->iteration}}. {{$pisci->name}} - {{$pisci->brojac}}</p>
                     @endforeach
                 </div>
                 <div>
-                    <h1>Upit 2</h1>
+                    <h1>Upit 2: Članci iz žanra Kultura u prvom mjesecu 2023 godine</h1>
                     <hr/>
                     @foreach($clanci as $clanci)
                     <p>{{$loop->iteration}}.  {{$clanci->name}}</p>
                     @endforeach
                 </div>
                 <div align="center"><br>
-                    <h1>Citaoci koji imaju najvise pretplata</h1>
+                    <h1>Upit 3: Citaoci koji imaju najvise pretplata</h1>
                     <hr/>
                     @foreach($csnp as $csnp)
                     <p>{{$loop->iteration}}.  {{$csnp->reader_id}}</p>
                     @endforeach
                 </div>
                 <div align="center"><br>
-                    <h1>Pretplate za odredjeni zanr u nekom periodu</h1>
+                    <h1>Upit 4: Pretplate za odredjeni zanr u nekom periodu</h1>
                     <hr/>
                     @foreach($pretplate as $pretplate)
                     <p>{{$loop->iteration}}.  {{$pretplate->datum}}</p>
                     @endforeach
                 </div>
                 <div align="center"><br>
-                    <h1>Pretplate za odredjeni zanr u nekom periodu nekog citaoca</h1>
+                    <h1>Upit 5: Pretplate za odredjeni zanr u nekom periodu nekog citaoca</h1>
                     <hr/>
                     @foreach($citaoci as $citaoci)
                     <p>{{$loop->iteration}}.  {{$citaoci->reader_id}}</p>
