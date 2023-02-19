7cc723d34c674fc170ba84ff7f9327a40170272d
Faris Veladzic <faris.veladzic@gmail.com>
2023-02-19 15:13:49 +0100

    Final2

--- resources/views/readers/index.blade.php
+++ resources/views/readers/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-2">
-                    <h1 class="font-xl">Zanrovi koji su dostupni kroz newslettere</h1>
+                    <h1 class="font-xl">Čitaoci koji su se prijavali za newslettere</h1>
                     @foreach ($readers as $reader)
                         <p class="p-2"> {{$reader->id}}: {{$reader->name}} - {{$reader -> mail}} - {{$reader->password}}</p>
                     @endforeach
